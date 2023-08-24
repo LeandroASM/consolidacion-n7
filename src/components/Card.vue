db2de6ffa508e985e60ddf756eea6b111079e5a6
Leandro Arias <leandro.arias.sm@gmail.com>
2023-08-24 01:47:27 -0400

    visualización de cards con los cursos, en el home

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,65 +1,70 @@
 <template>
-    <v-container>
-      <v-row  justify="space-around">
-        <v-card v-for="curso in cursos" :key="curso.id" width="400px">
-          <v-img
-            height="200"
-            :src="curso.img"
-            cover
-            class="text-white"
-          >
-            
-          </v-img>
-  
-          <v-card-text>
-            <div class="font-weight-bold ms-1 mb-2">
-              Today
-            </div>
-  
-            <v-timeline density="compact" align="start">
-              <v-timeline-item
-                v-for="message in messages"
-                :key="message.time"
-                :dot-color="message.color"
-                size="x-small"
-              >
-                <div class="mb-4">
-                  <div class="font-weight-normal">
-                    Costo: {{ curso.costo }}
-                  </div>
-                  <div>Duración: {{ curso.duracion}}</div>
-                </div>
-              </v-timeline-item>
-            </v-timeline>
-          </v-card-text>
-        </v-card>
-      </v-row>
-    </v-container>
-  </template>
+  <v-container>
+    <v-row justify="space-around">
+      <v-card width="300px">
+        <v-img height="200" :src="curso.img" cover class="text-white"> </v-img>
+
+        <v-card-title class="text-center">{{ curso.nombre }}</v-card-title>
+        <v-card-text>
+          <div class="font-weight-bold ms-1 mb-2">Información</div>
+
+          <v-timeline density="compact" align="start">
+            <v-timeline-item :dot-color="colors[0]" size="x-small">
+              <div class="mb-4">
+                <div class="font-weight-normal">Costo: {{ curso.costo }}</div>
+                <div>Duración: {{ curso.duracion }}</div>
+              </div>
+            </v-timeline-item>
+
+            <v-timeline-item :dot-color="colors[1]" size="x-small">
+              <div class="mb-4">
+                <div class="font-weight-normal">Cupos: {{ curso.cupos }}</div>
+                <div>Completado: {{ curso.completado }}</div>
+              </div>
+            </v-timeline-item>
+
+            <v-timeline-item :dot-color="colors[0]" size="x-small">
+              <div class="mb-4">
+                <div class="font-weight-normal">Fecha de Registro: {{ curso.fecha_registro }}</div>
+              </div>
+            </v-timeline-item>
+
+            <v-timeline-item :dot-color="colors[1]" size="x-small">
+              <div class="mb-4">
+                <div class="font-weight-normal">Descripción: {{ curso.descripcion }}</div>
+              </div>
+            </v-timeline-item>
+          </v-timeline>
+        </v-card-text>
+      </v-card>
+    </v-row>
+  </v-container>
+</template>
 
 <script>
 export default {
-    name: 'component-name',
-    props: {
-        cursos: {
-            type: Array,
-            required: true,
-        }
+  name: "card-component",
+  props: {
+    curso: {
+      type: Object,
+      required: true,
     },
-    data: function(){
-        return {}
-    },
-    // computed: {},
-    //methods: {}
-    // watch: {},
-    // components: {},
-    // mixins: [],
-    // filters: {},
-    // -- Lifecycle Methods
-    // -- End Lifecycle Methods
-}
+  },
+  data: function () {
+    return {
+      colors: ["purple-darken-1", "green-darken-1"],
+    };
+  },
+  // computed: {},
+  //methods: {}
+  // watch: {},
+  // components: {},
+  // mixins: [],
+  // filters: {},
+  // -- Lifecycle Methods
+  // -- End Lifecycle Methods
+};
 </script>
 
 <style scoped>
-    
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,12 @@
   <div>
     <NavBar/>
     <v-container>
-      <Card :cursos="cursos"/>
+      <v-row>
+        <v-col cols="12" md="6" lg="4" v-for="curso in cursos" :key="curso.id">
+          <Card :curso="curso"/>
+        </v-col>
+        
+      </v-row>
     </v-container>
   </div>
 </template>
