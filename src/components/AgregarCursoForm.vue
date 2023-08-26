8bf8de305d4657fc3bf073bba095a5f6235cdc53
Leandro Arias <leandro.arias.sm@gmail.com>
2023-08-26 02:46:32 -0400

    boton de agregar curso y despliegue de formulario

---
+++ src/components/AgregarCursoForm.vue
@@ -0,0 +1,112 @@
+<template>
+  <v-container class="mx-auto px-6 py-7">
+    <v-form ref="form" v-model="valid" lazy-validation>
+      <v-text-field
+        v-model="nombre"
+        :counter="10"
+        label="Nombre"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        v-model="img"
+        label="URL de la imagen"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        v-model="cupos"
+        label="Cupos del curso"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        v-model="inscritos"
+        label="Inscritos en el curso"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        v-model="duracion"
+        label="Inscritos en el curso"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        v-model="registro"
+        label="Fecha de registro"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        v-model="costo"
+        label="Costo del curso"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        v-model="descripcion"
+        label="Descripcion del curso"
+        required
+      ></v-text-field>
+
+      <v-btn color="success" class="mr-4" @click="addCurso()"> Agregar </v-btn>
+
+      <v-btn color="warning" class="mr-4" @click="reset()">
+        Limpiar Formulario
+      </v-btn>
+
+      <v-btn color="error" @click="hideForm()"> Cancelar </v-btn>
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "form-agregar-curso-component",
+  // props: {},
+  data: function () {
+    return {
+      nombre: "",
+      img: "",
+      cupos: undefined,
+      inscritos: undefined,
+      duracion: "",
+      registro: "",
+      costo: undefined,
+      completado: "false",
+      descripcion: "",
+    };
+  },
+  // computed: {},
+  methods: {
+    reset() {
+      this.$refs.form.reset();
+    },
+    addCurso(){
+            let nuevoCurso ={
+                nombre: this.nombre,
+                img: this.img,
+                cupos: parseInt(this.cupos),
+                duracion: this.duracion,
+                inscritos: parseInt(this.inscritos),
+                completado: this.completado,
+                fecha_registro: this.registro,
+                costo: parseInt(this.costo),
+                descripcion: this.descripcion,
+            }
+            this.$emit('agregarCurso',nuevoCurso)
+        },
+
+  },
+  // watch: {},
+  // components: {},
+  // mixins: [],
+  // filters: {},
+  // -- Lifecycle Methods
+  // -- End Lifecycle Methods
+};
+</script>
+
+<style scoped>
+</style>
--- src/views/AdministracionView.vue
+++ src/views/AdministracionView.vue
@@ -1,25 +1,44 @@
 <template>
-    <h1> Nuevo Componente</h1>
+  <div>
+    <v-row justify="center" class="mt-7">
+      <v-col cols="auto">
+        <v-dialog transition="dialog-bottom-transition" max-width="700px">
+          <template v-slot:activator="{ props }">
+            <v-btn variant="tonal" class="items-center" v-bind="props"
+              >Agregar Curso</v-btn
+            >
+          </template>
+          <template v-slot:>
+            <v-card>
+              <Form></Form>
+            </v-card>
+          </template>
+        </v-dialog>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
+import Form from "@/components/AgregarCursoForm.vue";
 export default {
-    name: 'Administracion-view',
-    // props: {},
-    data: function(){
-        return {}
-    },
-    // computed: {},
-    //methods: {}
-    // watch: {},
-    // components: {},
-    // mixins: [],
-    // filters: {},
-    // -- Lifecycle Methods
-    // -- End Lifecycle Methods
-}
+  name: "Administracion-view",
+  // props: {},
+  data: function () {
+    return {};
+  },
+  // computed: {},
+  //methods: {}
+  // watch: {},
+  components: {
+    Form,
+  },
+  // mixins: [],
+  // filters: {},
+  // -- Lifecycle Methods
+  // -- End Lifecycle Methods
+};
 </script>
 
 <style scoped>
-    
 </style>
