0173740f9993bb45b8acac74c988b66a2d6f818f
Leandro Arias <leandro.arias.sm@gmail.com>
2023-08-31 07:51:03 -0400

    tabla de cursos

---
+++ src/components/TablaCursos.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-container>
+    <v-table fixed-header height="400px">
+      <thead>
+        <tr>
+          <th>Curso</th>
+          <th>Cupos</th>
+          <th>Inscritos</th>
+          <th>Duración</th>
+          <th>Costo</th>
+          <th>Terminado</th>
+          <th>Fecha</th>
+          <th>Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="curso in listaCursos" :key="curso.id">
+          <td>{{ curso.nombre }}</td>
+          <td>{{ curso.cupos }}</td>
+          <td>{{ curso.inscritos }}</td>
+          <td>{{ curso.duracion }}</td>
+          <td class="text-center">
+            <p class="bg-green rounded-pill pa-2">${{ curso.costo.toLocaleString('es-CL') }}</p>
+          </td>
+          <td v-if="curso.completado == true" class="text-center"><p class="bg-info rounded-pill pa-2">Si</p></td>
+          <td v-else class="text-center"><p class="bg-blue-grey-lighten-3 rounded-pill pa-2">No</p></td>
+          <td>{{ curso.fecha_registro }}</td>
+          <td>
+            <div class="d-flex justify-space-around">
+              <v-icon color="yellow"
+                >mdi-pencil</v-icon
+              >
+              <v-icon color="red"
+                >mdi-delete</v-icon
+              >
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </v-table>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "dataTable-component",
+  props: {
+    listaCursos: {
+      type: Object,
+      required: true,
+    },
+  },
+  data: () => ({}),
+  // computed: {},
+  // methods: {},
+  // watch: {},
+  // components: {},
+  // mixins: [],
+  // filters: {},
+  // -- Lifecycle Methods
+
+  // -- End Lifecycle Methods
+};
+</script>
+
+<style scoped>
+</style>
--- src/views/AdministracionView.vue
+++ src/views/AdministracionView.vue
@@ -1,37 +1,49 @@
 <template>
   <div>
-    <v-row justify="center" class="mt-7">
-      <v-col cols="auto">
-        <v-dialog transition="dialog-bottom-transition" max-width="700px">
-          <template v-slot:activator="{ props }">
-            <v-btn variant="tonal" class="items-center" v-bind="props"
-              >Agregar Curso</v-btn
-            >
-          </template>
-          <template v-slot:>
-            <v-card>
-              <Form></Form>
-            </v-card>
-          </template>
-        </v-dialog>
-      </v-col>
-    </v-row>
+    <div>
+      <v-row justify="center" class="mt-7">
+        <v-col cols="auto">
+          <v-dialog transition="dialog-bottom-transition" max-width="700px">
+            <template v-slot:activator="{ props }">
+              <v-btn variant="tonal" class="items-center" v-bind="props"
+                >Agregar Curso</v-btn
+              >
+            </template>
+            <template v-slot>
+              <v-card>
+                <Form></Form>
+              </v-card>
+            </template>
+          </v-dialog>
+        </v-col>
+      </v-row>
+    </div>
+    <div>
+      
+        <TablaCursos :listaCursos="cursos"></TablaCursos>
+      
+    </div>
   </div>
 </template>
 
 <script>
 import Form from "@/components/AgregarCursoForm.vue";
+import TablaCursos from '@/components/TablaCursos.vue'
+import { mapState } from "vuex";
 export default {
   name: "Administracion-view",
   // props: {},
   data: function () {
     return {};
   },
-  // computed: {},
+  computed: {
+    ...mapState(['cursos']),
+  },
   //methods: {}
   // watch: {},
   components: {
     Form,
+    TablaCursos,
   },
   // mixins: [],
   // filters: {},
